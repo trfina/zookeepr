0064bde4fa58e8dfb2ce9bfa0ca223374bd76644
Tammy Fina <finatammy@gmail.com>
2022-01-12 16:23:59 -0500

    GET requests added for html pages

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const { animals } = require('./data/animals');
 const fs = require('fs');
 const path = require('path');
+const { ppid } = require('process');
+const res = require('express/lib/response');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static('public'));
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -113,6 +117,24 @@ app.post('/api/animals', (req,res) => {
         res.json(animal);
     }    
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
 
 app.listen(PORT, () => {
     console.log('API server now on port ${PORT}!'); 
